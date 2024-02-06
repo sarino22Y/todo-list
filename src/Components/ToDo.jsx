76768e591ddfe7e8706cdaf213dd383686568c26
Sarino MALALANIRINA <sarino.malalanirna@esti.mg>
2024-02-06 14:49:53 +0300

    Modifier le statut d'une tâche cliquée.

--- src/Components/ToDo.jsx
+++ src/Components/ToDo.jsx
@@ -10,10 +10,19 @@ const ToDo = () => {
     const add = () => {
         setTodos([...todos, {no: count++, text:inputRef.current.value, display:""}]);
         inputRef.current.value = "";
+        localStorage.setItem('todos_count', count);
     }
 
     useEffect(() => {
-        console.log(todos);
+        setTodos(JSON.parse(localStorage.getItem('todos')));
+        count = localStorage.getItem('todos_count');
+    }, []);
+
+    useEffect(() => {
+        setTimeout(() => {
+            console.log(todos);
+            localStorage.setItem('todos', JSON.stringify(todos))
+        }, 100);
     }, [todos]);
 
     
@@ -22,11 +31,11 @@ const ToDo = () => {
         <div className="todo-header">ToDo List</div>
         <div className="todo-add">
             <input ref={inputRef} type="text" name="" id="" className='todo-input' placeholder='Add your task'/>
-            <div onClick={() => {add()}} className="todo-add-btn">ADD</div>
+            <div onClick={() => {add();}} className="todo-add-btn">ADD</div>
         </div>
         <div className="todo-list">
             {todos.map((item, index)=> {
-                return <ToDoItems key={index} no={item.no} display={item.display} text={item.text} />
+                return <ToDoItems key={index} setTodos={setTodos} no={item.no} display={item.display} text={item.text} />
             })}
         </div>
     </div>
--- src/Components/ToDoItems.jsx
+++ src/Components/ToDoItems.jsx
@@ -1,21 +1,40 @@
-import './CSS/TodoItems.css'
-import tick from './assets/tick.png'
-import noTick from './assets/no-tick.png'
-import cross from './assets/x-btn.png'
+import "./CSS/TodoItems.css";
+import tick from "./assets/tick.png";
+import noTick from "./assets/no-tick.png";
+import cross from "./assets/x-btn.png";
 
-const ToDoItems = ({no, text, display}) => {
-  return (
-    <div>
-        <div className="todoitems">
-            <div className="todoitems-container">
-                <img src={tick} alt="" />
-                <img src={noTick} alt="" />
-                <div className="todoitems-text">{text}</div>
+const ToDoItems = ({ no, display, text, setTodos }) => {
+    
+    const toggle = (no) => {
+        let data = JSON.parse(localStorage.getItem('todos'));
+        for (let i = 0; i < data.length; i++) {
+            console.log("data[i].no" ,data[i].no);
+            console.log("no" , no);
+            if (data[i].no === no) {
+                if (data[i].display === "") {
+                    console.log("mitovy sy vide" , no);
+                    data[i].display = "line-through";
+                } else {
+                    data[i].display = "";
+                }
+                break;
+            }
+        }
+        console.log("data0" ,data);
+        setTodos(data);
+    }
+
+    return (
+        <div>
+            <div className="todoitems">
+                <div className={`todoitems-container ${display}`} onClick={()=>{toggle(no)}}>
+                    {display === "" ? (<img src={noTick} alt="" /> ) : (<img src={tick} alt="" />)}
+                    <div className="todoitems-text">{text}</div>
+                </div>
+                <img className="todoitems-cross-icon" src={cross} alt="" />
             </div>
-            <img className='todoitems-cross-icon' src={cross} alt="" />
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default ToDoItems
+export default ToDoItems;
